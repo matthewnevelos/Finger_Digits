1f4ad03cfb45b7c6b0cdb310588fab1bd65a4b91
Matthew <matthewnevelos@gmail.com>
2024-03-26 19:39:53 -0600

    added a4-production

--- a5-final-product.py
+++ a5-final-product.py
@@ -1,20 +1,206 @@
 # Adapted from code found in this thread:
 # https://stackoverflow.com/questions/32342935/using-opencv-with-tkinter
-
-from PIL import Image, ImageTk, ImageOps
+from PIL import Image, ImageTk, ImageOps, ImageDraw, ImageFont
 import tkinter as tk
 from tkinter import ttk
 import cv2
 import os
 import logging
-from tkinter import filedialog
+from tkinter import filedialog, colorchooser
 import threading
+from fastai.vision.all import *
 
 class ProductionFrame(tk.Toplevel):
-    pass
+    def __init__(self, parent):
+        """ Initialize frame which uses OpenCV + Tkinter. 
+            The frame:
+            - Uses OpenCV video capture and periodically captures an image.
+            - Uses a fastai learner to predict the finger count
+            - Overlays the label and probability on the image
+            - and shows it in Tkinter
+            
+            attributes:
+                vs (cv2 VideoSource): webcam to capture images from
+                learn (fastai Learner): CNN to generate prediction.
+                current_image (PIL Image): current image displayed
+                pil_font (PIL ImageFont): font for text overlay
+                panel (ttk Label): to display image in frame
+        """
+        super().__init__(parent)
+        
+        # 0 is your default video camera
+        self.vs = cv2.VideoCapture(0) 
+        
+
+        # Use CPU for predict
+        self.CPU = tk.BooleanVar()
+
+        # Check if Cuda PyTorch installed
+        if torch.cuda.is_available():
+            self.cuda = True
+            self.CPU.set(False)
+        else:
+            logging.warning("PyTorch Cuda not found")
+            self.cuda = False
+            self.CPU.set(True) 
+
+
+        self.choose_model()
+
+        
+        self.current_image = None 
+        self.pil_font = ImageFont.truetype("fonts/DejaVuSans.ttf", 40)
+
+
+        self.output_path = "screenshots/"
+        if not os.path.exists(self.output_path):
+            os.makedirs(self.output_path)
+
+        # Set colour of prob text
+        self.colour = 'black'
+
+
+        
+       
+
+        # FPS
+        self.fps_current = tk.StringVar()
+        self.fps_goal = tk.DoubleVar()
+        self.fps_goal.set(20)
+        
+        # Create widgets
+        # Panel shows image
+        self.panel = ttk.Label(self, image=self.current_image)  
+
+        # Save screenshot button
+        self.save_btn = ttk.Button(self, text="Save", command=self.screenshot)
+        
+        # Change model button
+        self.model_btn = ttk.Button(self, text="Change model", command=self.choose_model)
+
+        # Change colour button
+        self.colour_btn = ttk.Button(self, text="Change colour", command=self.change_colour)
+
+        # Use GPU checkbox
+        self.cpu_enable = ttk.Checkbutton(self, text="Use CPU", command=self.gpu_accel_toggle, variable=self.CPU)
+        if not self.cuda:
+            self.cpu_enable.config(state=tk.DISABLED)
+
+        # FPS
+        self.fps_current_label = ttk.Label(self, textvariable=self.fps_current)
+        self.fps_goal_text = ttk.Label(self, text="Target FPS: ")
+        self.fps_goal_entry = ttk.Entry(self, textvariable=self.fps_goal)
+        
+        # Order Widgets
+        self.save_btn.pack(fill='x')
+        self.model_btn.pack(fill='x')
+        self.colour_btn.pack(fill='x')
+        self.cpu_enable.pack(side="top")
+        self.fps_goal_text.pack()
+        self.fps_goal_entry.pack()
+        self.fps_current_label.pack()
+        self.panel.pack()
+
+        # start a self.video_loop that constantly pools the video sensor
+        # for the most recently read frame
+        self.video_loop(colour=self.colour)
+
+        
+    def video_loop(self, colour='red'):
+        """ Get frame from the video stream and show it in Tkinter 
+            
+            The image is processed using PIL: 
+            - crop left and right to make image smaller
+            - mirror 
+            - convert to Tkinter image
+            
+            Uses fastai learner to predict label and probability,
+            overlayed as text onto image displayed.
+            
+            Uses after() to call itself again after 30 msec.
+        
+        """
+        time1 = time.time()
+        # read frame from video stream
+        ok, frame = self.vs.read()  
+        # frame captured without any errors
+        if ok:  
+            # convert colors from BGR (opencv) to RGB (PIL)
+            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  
+            # convert image for PIL
+            self.current_image = Image.fromarray(cv2image)  
+            # camera is wide: crop 200 from left and right
+            # self.current_image = ImageOps.crop(self.current_image, (200,0,200,0)) 
+            # mirror, easier to locate objects
+            self.current_image = ImageOps.mirror(self.current_image) 
+            
+            #predict
+            pred,pred_idx,probs = self.learn.predict(tensor(self.current_image))
+            pred_str = f"{pred} ({probs[pred_idx].item():.2f})"
+    
+            #add text
+            draw = ImageDraw.Draw(self.current_image)
+            draw.text((10, 10), pred_str, font=self.pil_font, fill=colour)
+            
+            # convert image for tkinter for display, scale to 50% of size of original image
+            imgtk = ImageTk.PhotoImage(image=ImageOps.scale(self.current_image, 0.5)) 
+            # anchor imgtk so it does not get deleted by garbage-collector
+            self.panel.imgtk = imgtk  
+             # show the image
+            self.panel.config(image=imgtk)
+
+        time2 = time.time()
+        self.fps_current.set(f"FPS: {int(1/(time2-time1))}")
+        
+        # do this again after 30 milliseconds
+        self.after(int(1000/self.fps_goal.get()), self.video_loop, self.colour) 
+
+    def destructor(self):
+        """ Destroy the root object and release all resources """
+        logging.info("closing GUI...")
+        self.destroy()
+        self.vs.release()  # release web camera
+        cv2.destroyAllWindows()  # it is not mandatory in this application
+
+    def screenshot(self):
+        """Take screenshot and save to screenshot folder"""
+        pic_count = len(os.listdir(self.output_path))
+        file_name =  '0'*(8-len(str(pic_count+1))) + str(pic_count + 1) + ".jpg"
+        self.current_image.save(self.output_path + file_name)
+        logging.info("Saved screenshots\{}".format(file_name))
+
+    def choose_model(self):
+        """Change the learner used"""
+        self.model_path = filedialog.askopenfilename()
+        try:
+            self.learn = load_learner(self.model_path, cpu=self.CPU.get())
+            logging.info(f"learner {self.model_path} loaded")
+        except FileNotFoundError:
+            try:
+                self.learn==None
+                logging.warning(f"No model found. Resorting to previous model")
+
+            except AttributeError:
+                logging.error(f"No Model found")
+                self.choose_model()
+        except:
+            logging.error(f"Model {self.model_path} Could not be loaded. Please try again")
+            self.choose_model()
+
+
+        
+
+    def change_colour(self):
+        """Change the label colour"""
+        self.colour = colorchooser.askcolor(title="Choose Label Colour")[1]
+        logging.info(f"Colour changed to: {self.colour}")
+
+    def gpu_accel_toggle(self):
+        logging.info(f"Using CPU: {self.CPU.get()}")
+        self.learn = load_learner(self.model_path, cpu=self.CPU.get())
 
 class ImageCaptureFrame(tk.Toplevel):
-    def __init__(self, master):
+    def __init__(self, parent):
         """ Initialize frame which uses OpenCV + Tkinter. 
             The frame:
             - Uses OpenCV video capture and periodically captures an image
@@ -33,7 +219,7 @@ class ImageCaptureFrame(tk.Toplevel):
                 browse_btn (ttk Button): press to change directory
                 panel (ttk Label): to display image in frame
         """
-        super().__init__(master)
+        super().__init__(parent)
         self.grid()
 
         # Bool for freezing camera feed after picture is taken
@@ -174,10 +360,10 @@ class Menu(tk.Tk):
         self.title("ENDG 411 Finger Digits")
 
         self.capture_btn = ttk.Button(self, text="Open Image Capture", command=self.open_capture)
-        self.capture_btn.grid()
+        self.capture_btn.pack()
 
         self.production_btn = ttk.Button(self, text="Open Production", command=self.open_production)
-        self.production_btn.grid()
+        self.production_btn.pack()
         
 
     def open_capture(self):
@@ -223,6 +409,11 @@ class Menu(tk.Tk):
 def main():
     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
     app = Menu()
+    aw=400
+    ah=300
+    sw = app.winfo_screenwidth()
+    sh = app.winfo_screenheight()
+    app.geometry('%dx%d+%d+%d' % (aw, ah, (sw-aw)/2, (sh-ah)/2))
     app.mainloop()
         
 if __name__ == '__main__':
